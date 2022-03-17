89afd1760c320b218f806ec115848d77b0cb4ecf
Kjersti <kjkv@nmbu.no>
2022-03-17 16:01:34 +0100

    Added simple test of BreakoutState constructor

---
+++ Breakout - opgave 1/src/breakout/BreakoutStateTest.java
@@ -0,0 +1,34 @@
+package breakout;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BreakoutStateTest {
+
+	@Test
+	void test() {
+		int diameter = 5;
+		Point center = new Point(1,1);
+		Vector speed = new Vector(3,4); 
+		Point TL = new Point(5,5);
+		Point BR = new Point(8,8);
+		Point paddlecenter = new Point(10,10);
+		Vector paddlesize = new Vector(3,4);
+		
+		BallState[] balls = {new BallState(center, diameter, speed), 
+		                     new BallState(center, diameter+1, speed)};
+		BlockState[] blocks = {new BlockState(TL, BR)}; 
+		PaddleState paddle = new PaddleState(paddlecenter, paddlesize);
+		BreakoutState breakout = new BreakoutState(balls, blocks, BR, paddle);
+		
+		assertEquals(breakout.getBalls(), balls);
+		assertEquals(breakout.getBlocks(), blocks);
+		assertEquals(breakout.getPaddle(), paddle);
+		assertEquals(breakout.getBottomRight(), BR);
+		
+		
+		
+	}
+
+}
