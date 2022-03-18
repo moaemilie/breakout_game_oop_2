b117ba6e16bb33db0da8fd26fb476fbacf8b84b7
Kjersti <kjkv@nmbu.no>
2022-03-18 12:19:45 +0100

    Added test for getters

--- Breakout - opgave 1/src/breakout/BlockStateTest.java
+++ Breakout - opgave 1/src/breakout/BlockStateTest.java
@@ -11,6 +11,8 @@ class BlockStateTest {
 		Point top_left = new Point(0,0);
 		Point bottom_right = new Point(4,4);
 		BlockState block = new BlockState(top_left, bottom_right);
+		assertEquals(top_left, block.getTopLeft());
+		assertEquals(bottom_right, block.getBottomRight());
 	}
 
 }
