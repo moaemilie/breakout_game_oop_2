6dc511e6e0653241f6dcdfca34c0dc5764a0f874
Kjersti <kjkv@nmbu.no>
2022-03-18 12:19:54 +0100

    Added getters and documentation

--- Breakout - opgave 1/src/breakout/BlockState.java
+++ Breakout - opgave 1/src/breakout/BlockState.java
@@ -1,7 +1,9 @@
 package breakout;
 
 /**
- * This class represents a block with a gived top left point and botom right point.
+ * This class represents a block with a given top left point and bottom right point.
+ *  
+ *  @invar | getTopLeft().isUpAndLeftFrom(getBottomRight())
  *  
  * @immutable
  */
@@ -9,6 +11,9 @@ package breakout;
 public class BlockState {
 	// TODO: implement
 	// Contractual programming
+	/**
+	 * @invar | top_left.isUpAndLeftFrom(bottom_right)
+	 */
 	
 	private final Point top_left;
 	private final Point bottom_right;
@@ -16,15 +21,28 @@ public class BlockState {
 	/**
 	 * @pre | top_left.getX() >= 0 
 	 * @pre | top_left.getY() >= 0 
-	 * @pre | bottom_right.getY() > top_left.getY()
-	 * @pre | bottom_right.getX() > top_left.getX()
+	 * @pre | top_left.isUpAndLeftFrom(bottom_right)
+	 * @post | getTopLeft() == top_left
+	 * @post | getBottomRight() == bottom_right
 	 */
 	
 	public BlockState(Point top_left, Point bottom_right) {
 		this.top_left = top_left; 
 		this.bottom_right = bottom_right;
-
 	}
 	
+	/**
+	 * Returns top left corner of the block
+	 */
+	public Point getTopLeft() {
+		return top_left;
+	}
+	
+	/**
+	 * Returns bottom right corner of the block
+	 */
+	public Point getBottomRight() {
+		return bottom_right;
+	}
 	
 }
