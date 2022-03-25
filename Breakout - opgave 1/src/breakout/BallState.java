2d5d6aac82d35617c826b7a6fd1909219164e0d0
moaemilie <emilie.g.l@hotmail.com>
2022-03-25 10:21:45 +0100

    Removed preconditions for center coordinates

--- Breakout - opgave 1/src/breakout/BallState.java
+++ Breakout - opgave 1/src/breakout/BallState.java
@@ -12,8 +12,6 @@ package breakout;
 public class BallState {
 	
 	/**
-	 * @invar | center.getX() > 0
-	 * @invar | center.getY() > 0
 	 * @invar | diameter > 0
 	 */
 	private final Point center;
@@ -22,8 +20,6 @@ public class BallState {
 	
 	/**
 	 * @pre | diameter > 0 
-	 * @pre | center.getY() > 0
-	 * @pre | center.getX() > 0
 	 * 
 	 * @post | getCenter() == center
 	 * @post | getVelocity() == INIT_BALL_VELOCITY
