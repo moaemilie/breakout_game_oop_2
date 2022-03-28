4c1a1e9fff926ca0602977655dcda0f11f0993f6
moaemilie <emilie.g.l@hotmail.com>
2022-03-28 09:58:06 +0200

    Added bottom check

--- Breakout - opgave 1/src/breakout/BreakoutState.java
+++ Breakout - opgave 1/src/breakout/BreakoutState.java
@@ -1,6 +1,7 @@
 package breakout;
 
 import java.util.Arrays;
+import java.util.ArrayList; // import the ArrayList class
 import java.util.stream.IntStream;
 
 // TODO: implement, document
@@ -95,16 +96,15 @@ public class BreakoutState {
 	 * @mutates | this
 	 */
 	public void tick(int paddleDir) {
-		int i;
 		BallState[] newBalls = new BallState[balls.length];
-		for (i = 0; i < balls.length; i++) {
+		for (int i = 0; i < balls.length; i++) {
 			Vector velocity = balls[i].getVelocity();
 			Point center = balls[i].getCenter();
 			Point newCenter = new Point(center.getX() + velocity.getX(), center.getY() + velocity.getY());
 			newBalls[i] = new BallState(newCenter, balls[i].getDiameter(), velocity);
 		}
 		
-		for (i = 0; i < newBalls.length; i++) {
+		for (int i = 0; i < newBalls.length; i++) {
 			
 			// Checks if the ball hits the left wall and creates a mirrored ball
 			if(newBalls[i].getCenter().getX() - (newBalls[i].getDiameter()/2) <= 0 && newBalls[i].getVelocity().product(Vector.LEFT) > 0) {
@@ -119,20 +119,32 @@ public class BreakoutState {
 			}
 			
 			
-			
 			// Checks if the ball hits the top and creates a mirrored ball
 			if(newBalls[i].getCenter().getY() + (newBalls[i].getDiameter()/2) <= 0 && newBalls[i].getVelocity().product(Vector.UP) > 0) {
 				BallState newBallState = new BallState(newBalls[i].getCenter(),newBalls[i].getDiameter(), newBalls[i].getVelocity().mirrorOver(Vector.UP));
 				newBalls[i] = newBallState;
 			}
 			
-			
-		}
-			
 		
 		balls = newBalls;
 			
 	}
+
+		ArrayList<BallState> bottomBalls = new ArrayList<BallState>();
+		for (int i = 0 ;i < newBalls.length; i++) {
+			// Checks if the ball hits the bottom and removes the ball
+			if(newBalls[i].getCenter().getY() + (newBalls[i].getDiameter()/2) <= bottomRight.getY()) {
+				bottomBalls.add(newBalls[i]);
+			}
+		}
+		BallState[] newBottomBalls = new BallState[bottomBalls.size()];
+		newBottomBalls = bottomBalls.toArray(newBottomBalls);
+		balls = newBottomBalls;
+		
+		
+}
+		
+		
 	
 	/**
 	 * This method moves the paddle 10 steps to the right.
