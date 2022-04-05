2acca63b42b24f439252f96d371073507040a4bd
moaemilie <emilie.g.l@hotmail.com>
2022-04-05 10:37:27 +0200

    Changed the tests for the paddle

--- Breakout - opgave 1/src/breakout/BreakoutStateTest.java
+++ Breakout - opgave 1/src/breakout/BreakoutStateTest.java
@@ -32,7 +32,7 @@ class BreakoutStateTest {
 		
 		// Test that the velocity is updated after the tick method
 		BallState[] newBalls = {new BallState(new Point(4,4), diameter, speed)};
-		breakout.tick(2);
+		breakout.tick(0);
 		assert breakout.getBalls()[0].getCenter().equals(newBalls[0].getCenter());
 		assertEquals(breakout.getBalls()[0].getVelocity().getX(), newBalls[0].getVelocity().getX());
 		assertEquals(breakout.getBalls()[0].getVelocity().getY(), newBalls[0].getVelocity().getY());
@@ -52,7 +52,7 @@ class BreakoutStateTest {
 		Point BallNR = new Point(9,1);
 		BallState[] ballsNR = {new BallState(BallNR, diameter, speed)};
 		BreakoutState breakoutNR = new BreakoutState(ballsNR, blocks, BRMap, paddle);
-		breakoutNR.tick(1);
+		breakoutNR.tick(0);
 		assertEquals(breakoutNR.getBalls()[0].getVelocity().getX(), speed.mirrorOver(Vector.RIGHT).getX());
 		assertEquals(breakoutNR.getBalls()[0].getVelocity().getY(), speed.mirrorOver(Vector.RIGHT).getY());
 		
@@ -61,7 +61,7 @@ class BreakoutStateTest {
 		Vector speedNL = new Vector(-3, 3);
 		BallState[] ballsNL = {new BallState(BallNL, diameter, speedNL)};
 		BreakoutState breakoutNL = new BreakoutState(ballsNL, blocks, BRMap, paddle);
-		breakoutNL.tick(1);
+		breakoutNL.tick(0);
 		assertEquals(breakoutNL.getBalls()[0].getVelocity().getX(), speedNL.mirrorOver(Vector.LEFT).getX());
 		assertEquals(breakoutNL.getBalls()[0].getVelocity().getY(), speedNL.mirrorOver(Vector.LEFT).getY());
 		
@@ -70,7 +70,7 @@ class BreakoutStateTest {
 		Vector speedT = new Vector(0, -4);
 		BallState[] ballsT = {new BallState(BallT, diameter, speedT)};
 		BreakoutState breakoutT = new BreakoutState(ballsT, blocks, BRMap, paddle);
-		breakoutT.tick(1);
+		breakoutT.tick(0);
 		assertEquals(breakoutT.getBalls()[0].getVelocity().getX(), speedT.mirrorOver(Vector.UP).getX());
 		assertEquals(breakoutT.getBalls()[0].getVelocity().getY(), speedT.mirrorOver(Vector.UP).getY());		
 		
@@ -81,34 +81,36 @@ class BreakoutStateTest {
 		BallState[] ballsDown = {new BallState(BallDown, diameter, speedDown)};
 		BreakoutState breakoutDown = new BreakoutState(ballsDown, blocks, BRMap, paddle);
 		
-		breakoutDown.tick(1);
+		breakoutDown.tick(0);
 		
 		assert(breakoutNR.getBalls().length == 1);
 		assert(breakoutDown.getBalls().length == 0);
 		
+		
+		// TODO: Rewrite these test without empty lists.
 		// Checks if the game is won
 		BlockState[] blocksWon = {}; 
 		BreakoutState breakoutWon = new BreakoutState(balls, blocksWon, BRMap, paddle);
-		breakoutWon.tick(1);
+		breakoutWon.tick(0);
 		assert(breakoutWon.isWon() == true);
 		
 		// Check that the game is not won when there are no balls left.
 		BallState[] ballsNotWon = {};
 		BlockState[] blocksNotWon = {}; 
 		BreakoutState breakoutNotWon = new BreakoutState(ballsNotWon, blocksNotWon, BRMap, paddle);
-		breakoutNotWon.tick(1);
+		breakoutNotWon.tick(0);
 		assert(breakoutNotWon.isWon() == false);
 		
 		// Checks if the game is lost
 		BallState[] ballsDead = {};
 		BreakoutState breakoutDead = new BreakoutState(ballsDead, blocks, BRMap, paddle);
-		breakoutDead.tick(1);
+		breakoutDead.tick(0);
 		assert(breakoutDead.isDead() == true);
 		
 		// Checks that the game is not lost
 		BallState[] ballsNotDead = {new BallState(center, diameter, speed)};
 		BreakoutState breakoutNotDead = new BreakoutState(ballsNotDead, blocks, BRMap, paddle);
-		breakoutNotDead.tick(1);
+		breakoutNotDead.tick(0);
 		assert(breakoutNotDead.isDead() == false);
 				
 		// Test the intersection method
@@ -133,7 +135,7 @@ class BreakoutStateTest {
 		
 		Vector TestVectorTop = new Vector(2,2);
 		
-		interTop.tick(1);
+		interTop.tick(0);
 		assertEquals(interTop.getBalls()[0].getCenter().getX(), 4);
 		assertEquals(interTop.getBalls()[0].getCenter().getY(), 2);
 		assertEquals(interTop.getBalls()[0].getVelocity().getX(), TestVectorTop.mirrorOver(Vector.DOWN).getX());
@@ -150,7 +152,7 @@ class BreakoutStateTest {
 		
 		Vector TestVectorLeft = new Vector(2,2);
 		
-		interLeft.tick(1);
+		interLeft.tick(0);
 		assertEquals(interLeft.getBalls()[0].getCenter().getX(), 2);
 		assertEquals(interLeft.getBalls()[0].getCenter().getY(), 4);
 		assertEquals(interLeft.getBalls()[0].getVelocity().getX(), TestVectorLeft.mirrorOver(Vector.RIGHT).getX());
@@ -167,7 +169,7 @@ class BreakoutStateTest {
 		
 		Vector TestVectorDown = new Vector(0, 4);
 		
-		interDown.tick(1);
+		interDown.tick(0);
 		assertEquals(interDown.getBalls()[0].getCenter().getX(), 4);
 		assertEquals(interDown.getBalls()[0].getCenter().getY(), 2);
 		assertEquals(interDown.getBalls()[0].getVelocity().getX(), TestVectorDown.mirrorOver(Vector.UP).getX());
@@ -184,7 +186,7 @@ class BreakoutStateTest {
 		
 		Vector TestVectorHor = new Vector(-4, 0);
 		
-		interHor.tick(1);
+		interHor.tick(0);
 		assertEquals(interHor.getBalls()[0].getCenter().getX(), 6);
 		assertEquals(interHor.getBalls()[0].getCenter().getY(), 4);
 		assertEquals(interHor.getBalls()[0].getVelocity().getX(), TestVectorHor.mirrorOver(Vector.RIGHT).getX());
@@ -194,96 +196,35 @@ class BreakoutStateTest {
 		
 		//Checks if ball hits paddle and if ball has sped up
 		// Define objects 
-		int diameterB = 2;
-		Point TL_B = new Point(4,3);
-		Point BR_B = new Point(7,6);
-		Point BRMap_B = new Point(10,10);
-		Point paddlecenter_B = new Point(5,10);
-		Vector paddlesize_B = new Vector(1,1);
-		PaddleState paddle_B = new PaddleState(paddlecenter_B, paddlesize_B);
 		
+		int diameterP = 2;
+		Point BRMap_P = new Point(10,10);
 		
-		Point centerP = new Point(5, 8);
-		Vector velocityP = new Vector(0,1); 
-		Point paddlecenter_P = new Point(5,10);
-		Vector paddlesize_P = new Vector(3,1);
+		Point TL_B = new Point(0,0);
+		Point BR_B = new Point(1,1);
+		
+		Point centerP = new Point(5, 5);
+		Vector velocityP = new Vector(0,3); 
+		Point paddlecenter_P = new Point(5,8);
+		Vector paddlesize_P = new Vector(2,2);
 		PaddleState paddle_P = new PaddleState(paddlecenter_P, paddlesize_P);
-		BallState[] balls_P = {new BallState(centerP, diameterB, velocityP)};
+		BallState[] balls_P = {new BallState(centerP, diameterP, velocityP)};
 		BlockState[] blocks_P = {new BlockState(TL_B, BR_B)}; 
-		BreakoutState breakout_P = new BreakoutState(balls_P, blocks_P, BRMap_B, paddle_P);
+		BreakoutState breakout_PL = new BreakoutState(balls_P, blocks_P, BRMap_P, paddle_P);
+		int paddleDirLeft = -1;
+		int paddleDirRight = 1;
 		
-		breakout_P.tick(2);
+		// Tests when paddle moves to the left
+		breakout_PL.tick(paddleDirLeft);
+		assertEquals(breakout_PL.getBalls()[0].getVelocity().getX(), velocityP.mirrorOver(Vector.DOWN).getX() + (1/5 * paddleDirLeft));
+		assertEquals(breakout_PL.getBalls()[0].getVelocity().getY(), velocityP.mirrorOver(Vector.DOWN).getY());
 		
-		//assert(breakout_P.getBalls()[0].getVelocity().getX() < velocityP.getX());
-		assertEquals(breakout_P.getBalls()[0].getVelocity().getX(), velocityP.getX());
-		assertEquals(breakout_P.getBalls()[0].getVelocity().getY(), velocityP.getY());
+		// Test when the paddle moves to the right
+		BreakoutState breakout_PR = new BreakoutState(balls_P, blocks_P, BRMap_P, paddle_P);
+		breakout_PR.tick(paddleDirRight);
+		assertEquals(breakout_PR.getBalls()[0].getVelocity().getX(), velocityP.mirrorOver(Vector.DOWN).getX() + (1/5 * paddleDirRight));
+		assertEquals(breakout_PR.getBalls()[0].getVelocity().getY(), velocityP.mirrorOver(Vector.DOWN).getY());
 		
-		breakout_P.tick(2);
-		//assertEquals(breakout_P.getBalls()[0].getVelocity().getX(), velocityP.getX());
-		//assertEquals(breakout_P.getBalls()[0].getVelocity().getY(), velocityP.getY());
-		
-		
-		
-		
-		/*
-		 * Intersect interTop = new Intersect(x_first, y_first, x_afther, y_afther,
-		 * velocityX, velocityY, topLeft_x, topLeft_y, BottomRight_x, BottomRight_y);
-		 * assertEquals(interTop.WhereIntersect(), 4);
-		 * 
-		 * Intersect interLeft = new Intersect(1, 3, 4, 5, 2, 3, 2, 2, 6, 6);
-		 * assertEquals(interLeft.WhereIntersect(), 3);
-		 * 
-		 * 
-		 * Intersect interRight = new Intersect(7, 5, 5, 3, -2, -2, 2, 2, 6, 6);
-		 * assertEquals(interRight.WhereIntersect(), 4);
-		 * 
-		 * Intersect interBottomRight = new Intersect(6, 8, 4, 4, -2, -4, 2, 2, 6, 7);
-		 * assertEquals(interBottomRight.GetA(), 0);
-		 * assertEquals(interBottomRight.WhereIntersect(), 7);
-		 * 
-		 * Intersect interPlusMinus = new Intersect(7, 6, 5, 3, 1, -1, 2, 2, 6, 6);
-		 * assertEquals(interPlusMinus.GetA(), 0);
-		 * assertEquals(interPlusMinus.WhereIntersect(), 6);
-		 * 
-		 * 
-		 * Intersect interVertRight = new Intersect(4, 1, 4, 4, 0, 4, 2, 2, 6, 6);
-		 * assertEquals(interVertRight.WhereIntersect(), 4);
-		 * 
-		 * Intersect interHorDown = new Intersect(9, 4, 5, 4, -4, 0, 2, 2, 6, 6);
-		 * assertEquals(interHorDown.WhereIntersect(), 4);
-		 * 
-		 * 
-		 * 
-		 * Intersect interHorUp = new Intersect(4, 7, 4, 5, 0, -2, 2, 2, 6, 6);
-		 * assertEquals(interHorUp.WhereIntersect(), 4);
-		 * 
-		 * Intersect interVertLeft = new Intersect(1, 4, 4, 4, 3, 0, 2, 2, 6, 6);
-		 * assertEquals(interVertLeft.WhereIntersect(), 4);
-		 * 
-		 * Intersect interBottomLeft = new Intersect(2, 8, 5, 5, 3, -3, 2, 2, 6, 6);
-		 * assertEquals(interBottomLeft.GetA(), -1);
-		 * assertEquals(interBottomLeft.WhereIntersect(), 4);
-		 * 
-		 * Intersect interTopRound = new Intersect(3, 0, 4, 4, 1, 4, 2, 2, 6, 6);
-		 * assertEquals(interTopRound.WhereIntersect(), 2);
-		 * 
-		 * 
-		 * 
-		 * Intersect interTopRight = new Intersect(6, 1, 4, 3, -2, 2, 2, 2, 6, 6);
-		 * assertEquals(interTopRight.WhereIntersect(), 5);
-		 * 
-		 * Intersect interRightLeft = new Intersect(7, 3, 5, 5, -2, 2, 2, 2, 6, 6);
-		 * assertEquals(interRightLeft.WhereIntersect(), 4);
-		 * 
-		 * Intersect interBottomLeftNeg = new Intersect(5, 7, 3, 5, -2, -2, 2, 2, 6, 6);
-		 * assertEquals(interBottomLeftNeg.WhereIntersect(), 4);
-		 * 
-		 * Intersect interLeftRight = new Intersect(1, 5, 3, 3, 2, -2, 2, 2, 6, 6);
-		 * assertEquals(interLeftRight.WhereIntersect(), 4);
-		 * 
-		 * Intersect interWrong = new Intersect(1, 5, 3, 3, 0, 0, 2, 2, 6, 6);
-		 * assertEquals(interWrong.WhereIntersect(), 0);
-		 */
 		
 	}
 	
