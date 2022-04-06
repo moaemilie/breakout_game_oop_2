32896e4bff0045a4baa7a9320420664ddee3fd6e
Kjersti <kjkv@nmbu.no>
2022-04-06 13:57:16 +0200

    Added tests for new gets

--- Breakout - opgave 1/src/breakout/BallStateTest.java
+++ Breakout - opgave 1/src/breakout/BallStateTest.java
@@ -17,6 +17,17 @@ class BallStateTest {
 		assertEquals(ball.getCenter(), center);
 		assertEquals(ball.getVelocity(), speed);
 	}
+	
+	@Test
+	void testTopLeft() {
+		int diameter = 10;
+		Point center = new Point(6,6);
+		Vector speed = new Vector(3,4);
+		BallState ball = new BallState(center, diameter, speed);
+		
+		assertEquals(new Point(1,1), ball.getTopLeft());
+		assertEquals(new Point(11,11), ball.getBottomRight());
+	}
 
 }
 
